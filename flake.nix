041dbcd52b0e87dde80d5f8543a3ff612a21e0cd
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-05-10 19:02:13 +0200

    feat: base flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "RémiOS 23.11";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    mkSystem = host: system: nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/${host}/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs      = true;
+          home-manager.useUserPackages    = true;
+          home-manager.extraSpecialArgs   = { inherit inputs; };
+          home-manager.users.remi         = import ./home/remi.nix;
+        }
+      ];
+    };
+  in {
+    nixosConfigurations = {
+      thinkpad = mkSystem "thinkpad" "x86_64-linux";
+      dell     = mkSystem "dell"     "x86_64-linux";
+      # server   = mkSystem "server"   "x86_64-linux";
+    };
+  };
+}
