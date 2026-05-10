1c3d9aee8a0d2ec6a29ba39d5b69496aab376720
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-05-10 21:45:42 +0200

    fix(sway): switch from pactl to wpctl for sound management
    pactl wasn't installed

--- home/sway.nix
+++ home/sway.nix
@@ -61,9 +61,9 @@ in
           "${mod}+Shift+Print" =
             "exec grim -g \"$(slurp)\" ~/media/screenshots/screenshot-$(date +%Y%m%d-%H%M%S).png";
 
-          "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-          "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-          "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+          "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
           "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
           "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
           "XF86AudioPlay" = "exec playerctl play-pause";
