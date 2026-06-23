c49fce3572a2a80fd9b9b2b048d2afc92835f8bf
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-06-23 11:39:00 +0200

    feat: add systemd-boot

--- hosts/dell/configuration.nix
+++ hosts/dell/configuration.nix
@@ -18,7 +18,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.initrd.systemd.enable = true;
   boot.initrd.kernelModules = [ "i915" ];
 
   hardware.graphics = {
