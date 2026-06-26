ae6e688ab3be4d8e6711a17396bad8c03053b89c
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-06-26 13:50:01 +0200

    feat(beets): add config

--- home/remi.nix
+++ home/remi.nix
@@ -138,6 +138,46 @@
     };
   };
 
+  # ── Beets ─────────────────────────────────────────────────────────────
+
+  programs.beets = {
+    enable = true;
+    settings = {
+      directory = "~/media/music";
+      library = "~/media/music/musiclibrary.db";
+      original_date = true;
+      per_disc_numbering = true;
+      asciify_path = true;
+      import = {
+        copy = "yes";
+      };
+      plugins = [
+        "musicbrainz"
+        "deezer"
+        "embedart"
+        "lyrics"
+        "fetchart"
+      ];
+      fetchart = {
+        auto = true;
+        cautious = false;
+        minwidth = 1000;
+        maxwidth = 3000;
+        sources = [
+          "filesystem"
+          "coverart"
+          "itunes"
+          "albumart"
+        ];
+      };
+      lyrics = {
+        auto = false;
+        sources = "*";
+        synced = true;
+      };
+    };
+  };
+
   # ── Theming ───────────────────────────────────────────────────────────
   gtk = {
     enable = true;
