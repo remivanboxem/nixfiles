b98edeb372a43dce48ea622e2c09ea03e26239c0
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-07-07 14:06:20 +0200

    feat(power): replace tlp to power-profiles-deamon

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -12,19 +12,23 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 
-  services.tlp = {
+  services.power-profiles-daemon = {
     enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
-      WIFI_PWR_ON_AC = "off";
-      WIFI_PWR_ON_BAT = "on";
-    };
   };
+
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #     START_CHARGE_THRESH_BAT0 = 75;
+  #     STOP_CHARGE_THRESH_BAT0 = 80;
+  #     WIFI_PWR_ON_AC = "off";
+  #     WIFI_PWR_ON_BAT = "on";
+  #   };
+  # };
 
   services.logind = {
     lidSwitch = "suspend";
