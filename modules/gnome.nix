90fd2a29e8327c849f2e619dc89376334c5ae082
Rémi Van Boxem <remi.vanboxem@uclouvain.be>
2026-07-07 14:00:33 +0200

    feat(gnome): enable basic GNOME

---
+++ modules/gnome.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # To disable installing GNOME's suite of applications
+  # and only be left with GNOME shell.
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+  ];
+
+}
